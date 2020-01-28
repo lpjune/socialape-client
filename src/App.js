e12d85aea0093293667abcce3eec57cd9cbdbd87
lpjune <laurenporsini@gmail.com>
2020-01-28 17:05:00 -0600

    #15 scream card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -10,13 +12,33 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: "#33c9dc",
+            main: "#00bcd4",
+            dark: "#008394",
+            contrastText: "#fff"
+        },
+        secondary: {
+            light: "#ff6333",
+            main: "#ff3d00",
+            dark: "#b22a00",
+            contrastText: "#fff"
+        }
+    },
+    typography: {
+        useNextVariants: true,
+    }
+});
+
 function App() {
     return (
-        <div className="App">
+        <MuiThemeProvider theme={theme}>
+            <div className="App">
             <Router>
-            <Navbar />
+                <Navbar />
                 <div className="container">
-                    
                     <Switch>
                         <Route exact path="/" component={home}></Route>
                         <Route exact path="/login" component={login}></Route>
@@ -25,6 +47,7 @@ function App() {
                 </div>
             </Router>
         </div>
+        </MuiThemeProvider>
     );
 }
 
