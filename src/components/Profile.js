8f8d9c89f31e442db5e814d11f86062cb470bef7
lpjune <laurenporsini@gmail.com>
2020-02-09 01:28:46 -0600

    #22 Logout and edit profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,26 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
-import { connect } from "react-redux";
-import MuiLink from '@material-ui/core/Link';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
+// MUI Stuff
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import MuiLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+// Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
-import dayjs from 'dayjs';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
-import Tooltip from '@material-ui/core/Tooltip';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+// Redux
+import { connect } from "react-redux";
 import { logoutUser, uploadImage } from '../redux/actions/userActions';
+
 
 const styles = (theme) => ({
     paper: {
@@ -75,6 +81,9 @@ class Profile extends Component {
         const fileInput = document.getElementById('imageInput');
         fileInput.click();
     }
+    handleLogout = () => {
+        this.props.logoutUser();
+    }
     render() {
         const {
             classes,
@@ -130,6 +139,12 @@ class Profile extends Component {
                         <CalendarToday color="primary"/>{' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+                    <Tooltip title="Logout" placement="top">
+                        <IconButton onClick={this.handleLogout}>
+                            <KeyboardReturn color="primary" />
+                        </IconButton>
+                    </Tooltip>
+                    <EditDetails />
                 </div>
             </Paper>
         ) : (
