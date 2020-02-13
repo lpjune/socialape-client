a887daaa2aef14c3f230e31d12246d5f8a75b78f
lpjune <laurenporsini@gmail.com>
2020-02-12 20:30:34 -0600

    #28 scream dialog details

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from "../util/MyButton";
+import LikeButton from "./LikeButton";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
 // MUI Stuff
@@ -13,29 +14,38 @@ import Typography from "@material-ui/core/Typography";
 // Icons
 import CloseIcon from "@material-ui/icons/Close";
 import UnfoldMore from "@material-ui/icons/UnfoldMore";
-// Redux
+import ChatIcon from "@material-ui/icons/Chat";
+// Redux stuff
 import { connect } from "react-redux";
 import { getScream } from "../redux/actions/dataActions";
 
 const styles = theme => ({
     ...theme.spreadThis,
     invisibleSeparator: {
-        border: 'none',
+        border: "none",
         margin: 4
     },
     profileImage: {
         maxWidth: 200,
         height: 200,
-        borderRadius: '50%',
-        objectFit: 'cover'
+        borderRadius: "50%",
+        objectFit: "cover"
     },
     dialogContent: {
         padding: 20
     },
     closeButton: {
         position: "absolute",
-        left: "91%",
-        top: "6%"
+        left: "90%"
+    },
+    expandButton: {
+        position: "absolute",
+        left: "90%"
+    },
+    spinnerDiv: {
+        textAlign: "center",
+        marginTop: 50,
+        marginBottom: 50
     }
 });
 
@@ -50,6 +60,7 @@ class ScreamDialog extends Component {
     handleClose = () => {
         this.setState({ open: false });
     };
+
     render() {
         const {
             classes,
@@ -64,28 +75,44 @@ class ScreamDialog extends Component {
             },
             UI: { loading }
         } = this.props;
+
         const dialogMarkup = loading ? (
-            <CircularProgress size={200} />
+            <div className={classes.spinnerDiv}>
+                <CircularProgress size={200} thickness={2} />
+            </div>
         ) : (
             <Grid container spacing={16}>
                 <Grid item sm={5}>
-                    <img src={userImage} alt="Profile" className={classes.profileImage} />
+                    <img
+                        src={userImage}
+                        alt="Profile"
+                        className={classes.profileImage}
+                    />
                 </Grid>
                 <Grid item sm={7}>
-                    <Typography component={Link} color="primary" variant="h5" to={`/users/${userHandle}`}>
+                    <Typography
+                        component={Link}
+                        color="primary"
+                        variant="h5"
+                        to={`/users/${userHandle}`}
+                    >
                         @{userHandle}
                     </Typography>
-                    <hr className={classes.invisibleSeparator}/>
+                    <hr className={classes.invisibleSeparator} />
                     <Typography variant="body2" color="textSecondary">
-                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                        {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
                     </Typography>
                     <hr className={classes.invisibleSeparator} />
-                    <Typography variant="body1">
-                        {body}
-                    </Typography>
+                    <Typography variant="body1">{body}</Typography>
+                    <LikeButton screamId={screamId} />
+                    <span>{likeCount} likes</span>
+                    <MyButton tip="comments">
+                        <ChatIcon color="primary" />
+                    </MyButton>
+                    <span>{commentCount} comments</span>
                 </Grid>
             </Grid>
-        )
+        );
         return (
             <Fragment>
                 <MyButton
